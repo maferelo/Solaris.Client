87709ed4f2dadfbca4231e6e3472d4f7adf73a7f
Felipe Restrepo <maferelo@outlook.com>
2024-03-31 13:00:27 -0500

    feat(sb): Add theme provider to storybook preview

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { ThemeProvider } from "@rneui/themed";
 
 import type { Preview } from "@storybook/react";
 import { View } from "react-native";
+import { theme } from "../src/config/theme";
 
 const preview: Preview = {
   parameters: {
@@ -15,9 +17,13 @@ const preview: Preview = {
 
   decorators: [
     (Story) => (
-      <View style={{ alignItems: "center", justifyContent: "center", flex: 1 }}>
-        <Story />
-      </View>
+      <ThemeProvider theme={theme}>
+        <View
+          style={{ alignItems: "center", justifyContent: "center", flex: 1 }}
+        >
+          <Story />
+        </View>
+      </ThemeProvider>
     ),
   ],
 };
