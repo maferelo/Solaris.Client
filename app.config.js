e56e164485c9ee870eb8a768dc4a109ca846740f
Felpe <maferelo13@gmail.com>
2024-09-24 12:04:32 -0500

    test expo-build-properties

--- app.config.js
+++ app.config.js
@@ -5,6 +5,16 @@ module.exports = ({ config }) => {
       ...config.extra,
       storybookEnabled: process.env.STORYBOOK_ENABLED || "false",
     },
-    plugins: ["expo-secure-store"],
+    plugins: [
+      "expo-secure-store",
+      [
+        "expo-build-properties",
+        {
+          ios: {
+            useFrameworks: "static",
+          },
+        },
+      ],
+    ],
   };
 };
